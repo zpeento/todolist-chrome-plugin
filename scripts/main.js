f859bd0526ea71c72be141f77729125f429f98ec
moose.zhou <509887158@qq.com>
2017-08-21 17:13:08 +0800

    change:js基本交互逻辑

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    console.log(0)
     var date = new Date();
     var curMonth = date.getMonth()+1;
     var curDay = date.getDate();
@@ -12,4 +13,50 @@ window.onload = function() {
     //title DOM数据初始化
     dateDom.innerHTML = curMonth + " 月 " + curDay + " 日 ";
     weekDom.innerHTML = weekList[curWeekDay];
+
+    //点击这个按钮会显示输入框
+    var addItemButton = document.querySelector(".addItemButton");
+
+    //输入计划框的容器
+    var addItemContainer = document.querySelector(".addItemContainer");
+
+    //点击这个按钮增加计划
+    var addItemShow = document.querySelector("#addItemShow");
+
+    //计划列表
+    var list = document.querySelector("#list");
+
+    //输入框
+    var addItemInput = document.querySelector("#addItemValue");
+
+    addItemButton.onclick = function() {
+        addItemButton.style.display = "none";
+        addItemContainer.style.display = "flex";
+    }
+
+    addItemShow.onclick = function() {
+
+        var newLiTag = document.createElement("li");
+
+        var newTaskTag = document.createElement("p");
+        var newTaskText = document.createTextNode(addItemInput.value);
+        newTaskTag.appendChild(newTaskText);
+        newLiTag.appendChild(newTaskTag);
+
+        var newCancelButton = document.createElement("button");
+        newCancelButton.innerHTML = '<button>x</button>';
+        newLiTag.appendChild(newCancelButton);
+
+        var newConfirmButton = document.createElement("button");
+        newConfirmButton.innerHTML = '<button>√</button>';
+        newLiTag.appendChild(newConfirmButton);
+
+        list.appendChild(newLiTag);
+
+        //隐藏输入框
+        addItemContainer.style.display = "none";
+
+        //显示添加计划按钮
+        addItemButton.style.display = "block";
+    }
 }
